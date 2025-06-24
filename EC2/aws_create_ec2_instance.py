4edb7f5547a30e1323b5e4fa2bd1c125f978fc0b
Nitesh Wayafalkar <niteshwayafalkar@yahoo.com>
2025-06-24 23:53:03 +0530

    feat: added create EC2 instance

--- EC2/aws_create_ec2_instance.py
+++ EC2/aws_create_ec2_instance.py
@@ -1,4 +1,35 @@
 import boto3
 
 class EC2InstanceCreator:
-    def __int__(self, 
+    def __init__(self, name, region, instance_type, key_name, security_groups_ids, image_id):
+        self.name = name
+        self.region = region
+        self.instance_type = instance_type
+        self.key_name = key_name
+        self.security_groups_ids = security_groups_ids
+        self.image_id = image_id
+
+    def create_instance(self):
+        response = boto3.client("ec2", region_name=self.region).run_instances(
+            ImageId=self.image_id,
+            InstanceType=self.instance_type,
+            KeyName=self.key_name,
+            SecurityGroupIds=self.security_groups_ids,
+            MinCount=1,
+            MaxCount=1,
+            TagSpecifications=[
+                {
+                    'ResourceType': 'instance',
+                    'Tags': [
+                        {
+                            'Key': 'Name',
+                            'Value': self.name
+                        }
+                    ]
+                }
+            ]
+        )
+
+obj =  EC2InstanceCreator("server_boto3_demo" , "ap-south-1", "t2.micro", "ssh_key_ap", ["sg-0c96e79e1d37f5551"], "ami-0f918f7e67a3323f0")
+print("Creating instance...")
+obj.create_instance()
